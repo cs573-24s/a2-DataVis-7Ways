de10134ecce001eadb390716218032666bd75f10
chikpea123 <mcliao@wpi.edu>
2024-02-08 18:55:11 -0500

    Writing updates

--- R+ggplot2/RVis.R
+++ R+ggplot2/RVis.R
@@ -11,7 +11,10 @@ p <- ggplot(df, aes(x = flipper_length_mm, y = body_mass_g)) +
   labs(x = "Flipper Length (mm)", y = "Body Mass (g)", size = "Bill Length (mm)") +
   theme_minimal() +
   scale_color_manual(values = c("blue", "red", "green")) +
-  guides(color = guide_legend(title = "Species"), size = "none")
+  guides(color = guide_legend(title = "Species"), size = "none") +
+  theme(axis.title.x = element_text(face = "bold", margin = margin(t = 40, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0))) 
+
 
 # Convert ggplot to plotly for hover features
 p <- ggplotly(p, tooltip = c("species", "flipper_length_mm", "body_mass_g", "bill_length_mm"),
