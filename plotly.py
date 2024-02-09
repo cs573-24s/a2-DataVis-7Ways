7f483c6c2727bc1daa8fdd783b6f3a453155d2b8
danile981199 <63995138+danile981199@users.noreply.github.com>
2024-02-08 23:22:31 -0500

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -1,18 +1,20 @@
-import pandas as pd
 import plotly.express as px
 
 # Load the data
 file_path = '/Users/a981199/Desktop/data_visualize/A2/R/a2/penglings.csv'
 data = pd.read_csv(file_path)
 
+# Handle NaN values in 'size' column
+data['bill_length_mm'] = data['bill_length_mm'].fillna(5)
+
 # Create the scatterplot
 fig = px.scatter(data, x='flipper_length_mm', y='body_mass_g', color='species',
                  size='bill_length_mm', opacity=0.8, template='plotly_dark',
-                 title='Penguin Scatterplot', labels={'Flipper Length': 'Flipper Length (mm)', 'Body Mass': 'Body Mass (g)'})
+                 title='Penguin Scatterplot', labels={'flipper_length_mm': 'Flipper Length (mm)', 'body_mass_g': 'Body Mass (g)'})
 
 # Customize layout
-fig.update_xaxes(tick0=10, dtick=10)
-fig.update_yaxes(tick0=10, dtick=10)
+fig.update_xaxes(tick0=170, dtick=10)
+fig.update_yaxes(tick0=3000, dtick=500)
 fig.update_layout(xaxis_title='Flipper Length', yaxis_title='Body Mass')
 
 # Show the plot
